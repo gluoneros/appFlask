5d20bfd18691f8be983562fec91deafa9e9d716d
gluoneros <gluoneros@gmail.com>
2023-12-19 22:54:00 -0500

    avance 19/12/23

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Comprar drs', 'enviar solicitud', 'enviar']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
-    return 'su ip es {}'.format(user_ip)
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+    return render_template('hello.html', **context)
