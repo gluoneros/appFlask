25aa4f390e4f585f609eadfd00300fd98338ed4a
gluoneros <gluoneros@gmail.com>
2023-12-22 23:07:26 -0500

    avance 22/12-23

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos = ['Comprar drs', 'enviar solicitud', 'enviar']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
@@ -28,3 +36,4 @@ def hello():
     }
 
     return render_template('hello.html', **context)
+
