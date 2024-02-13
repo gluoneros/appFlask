ae566150bdd4ea4b62abf8333f34fd794ca35f79
gluoneros <gluoneros@gmail.com>
2024-02-12 22:34:57 -0500

    Avance 12/02/24

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, request, make_response, redirect, render_template
-from flask_bootstrap import Bootstrap
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
+
+app = create_app()
+
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
@@ -22,18 +31,31 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
 
