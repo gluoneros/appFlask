93f45f0f4fc17a04d92649dcd93280d804e38f38
gluoneros <gluoneros@gmail.com>
2024-02-22 17:21:52 -0500

    new

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required
+
 
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 app = create_app()
-
-
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -36,26 +36,17 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
-        'todos': todos,
-        'login_form': login_form,
+        'todos': get_todos(user_id=username),
         'username': username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usario registrado con éxito!')
-
-        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
 
